ef1a9be439674b06ebe4e15522597924b51fc2a0
jjwils <mail@johnwilson.me.uk>
2024-05-21 13:31:42 +0100

    R**

--- src/test/java/train/the/trainer/FizzBuzzTest.java
+++ src/test/java/train/the/trainer/FizzBuzzTest.java
@@ -9,31 +9,36 @@ class FizzBuzzTest {
     @Test
     void return_1_given_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("1", fizzBuzz.convert(1));
+        String actual = fizzBuzz.convert(1);
+        assertEquals("1", actual);
     }
 
     @Test
     void return_2_given_2() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("2", fizzBuzz.convert(2));
+        String actual = fizzBuzz.convert(2);
+        assertEquals("2", actual);
     }
 
     @Test
     void return_fizz_given_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.convert(3));
+        String actual = fizzBuzz.convert(3);
+        assertEquals("Fizz", actual);
     }
 
     @Test
     void return_buzz_given_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Buzz", fizzBuzz.convert(5));
+        String actual = fizzBuzz.convert(5);
+        assertEquals("Buzz", actual);
     }
 
     @Test
     void return_fizzbuzz_given_15() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("FizzBuzz", fizzBuzz.convert(15));
+        String actual = fizzBuzz.convert(15);
+        assertEquals("Buzz", fizzBuzz.convert(5));
     }
 
 }
