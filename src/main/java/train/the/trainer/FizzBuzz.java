71623ab8a900ccb522ede8f946e72df3677e7d61
jjwils <mail@johnwilson.me.uk>
2024-05-21 13:20:34 +0100

    R** change name

--- src/main/java/train/the/trainer/FizzBuzz.java
+++ src/main/java/train/the/trainer/FizzBuzz.java
@@ -1,7 +1,7 @@
 package train.the.trainer;
 
 public class FizzBuzz {
-    public String fizzBuzz(int number) {
+    public String convert(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
         } else if (number % 3 == 0) {
--- src/test/java/train/the/trainer/FizzBuzzTest.java
+++ src/test/java/train/the/trainer/FizzBuzzTest.java
@@ -7,14 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     @Test
-    void fizzBuzz() {
+    void return_1_given_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
-        assertEquals("Fizz", fizzBuzz.fizzBuzz(3));
-        assertEquals("Buzz", fizzBuzz.fizzBuzz(5));
+        assertEquals("1", fizzBuzz.convert(1));
+    }
 
-        assertEquals("1", fizzBuzz.fizzBuzz(1));
+    @Test
+    void return_2_given_2() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("2", fizzBuzz.convert(2));
+    }
 
+    @Test
+    void return_fizz_given_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+
+    @Test
+    void return_buzz_given_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.convert(5));
+    }
+
+    @Test
+    void return_fizzbuzz_given_15() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("FizzBuzz", fizzBuzz.convert(15));
     }
 
 }
