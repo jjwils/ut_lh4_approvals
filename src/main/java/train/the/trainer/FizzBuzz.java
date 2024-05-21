9f69e000acd8fad622958c9b8c7c311546a05363
jjwils <mail@johnwilson.me.uk>
2024-05-21 13:45:11 +0100

    R**

--- src/main/java/train/the/trainer/FizzBuzz.java
+++ src/main/java/train/the/trainer/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
     public String convert(int number) {
         if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
-        } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+        } else if (number % 3 == 0) {
             return "Fizz";
         } else if (number % 5 == 0) {
             return "Buzz";
