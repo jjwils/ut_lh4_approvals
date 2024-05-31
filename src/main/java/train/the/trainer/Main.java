29b6998c1725dbffe30c1cb47a3ad02bd180f08c
jjwils <mail@johnwilson.me.uk>
2024-05-31 10:17:31 +0100

    F** - production code print

---
+++ src/main/java/train/the/trainer/Main.java
@@ -0,0 +1,9 @@
+package train.the.trainer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Program program = new Program(new Printer());
+        program.print("1");
+    }
+}
