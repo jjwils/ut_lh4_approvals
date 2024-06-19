c21f3680b88c86cd1cdea06e33681f1128e62baf
jjwils <mail@johnwilson.me.uk>
2024-06-19 08:56:12 +0100

    r - rename method

--- src/main/java/train/the/trainer/Main.java
+++ src/main/java/train/the/trainer/Main.java
@@ -28,7 +28,7 @@ public class Main {
                     System.out.println(hotels[j]);
                 }
                 System.out.println("");
-                app.updateQuality();
+                app.update();
             }
         }
     }
