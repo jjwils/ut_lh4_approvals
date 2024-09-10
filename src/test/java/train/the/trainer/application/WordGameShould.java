bef3994259383a63fd07c1c4575267d6621a07a8
John Wilson <mail@johnwilson.me.uk>
2024-09-10 08:39:45 +0100

    r - rename wordgameservice to wordgame

--- src/test/java/train/the/trainer/application/WordGameShould.java
+++ src/test/java/train/the/trainer/application/WordGameShould.java
@@ -1,23 +1,22 @@
 package train.the.trainer.application;
 
 import org.junit.jupiter.api.Test;
-import train.the.trainer.application.WordGameService;
 import train.the.trainer.application.port.TestDoublePrinter;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class WordGameServiceShould {
+public class WordGameShould {
 
 
     @Test
     void print_numbers(){
 
         TestDoublePrinter testDoublePrinter = new TestDoublePrinter();
-        WordGameService wordGameService = new WordGameService(testDoublePrinter);
+        WordGame wordGame = new WordGame(testDoublePrinter);
 
-        wordGameService.printNumbers(2);
+        wordGame.printNumbers(2);
 
         assertEquals(2, testDoublePrinter.count());
     }
