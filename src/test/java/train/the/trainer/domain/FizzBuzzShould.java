2e17ba7dddb6dff6e656a2aba01d9669914ad17b
John Wilson <mail@johnwilson.me.uk>
2024-09-10 08:08:48 +0100

    r - change to param test

--- src/test/java/train/the/trainer/domain/FizzBuzzShould.java
+++ src/test/java/train/the/trainer/domain/FizzBuzzShould.java
@@ -1,63 +1,25 @@
 package train.the.trainer.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FizzBuzzShould {
+class FizzBuzzShould  {
 
-    @Test
-    void return_1_given_1() {
+    @ParameterizedTest
+    @CsvSource({
+            "1, '1'",
+            "2, '2'",
+            "3, 'Fizz'",
+            "6, 'Fizz'",
+            "5, 'Buzz'",
+            "10, 'Buzz'",
+            "15, 'FizzBuzz'",
+            "30, 'FizzBuzz'"
+    })
+    void testFizzBuzz(int input, String expectedOutput) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(1);
-        assertEquals("1", actual);
+        assertEquals(expectedOutput, fizzBuzz.convert(input));
     }
-
-    @Test
-    void return_2_given_2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(2);
-        assertEquals("2", actual);
-    }
-
-    @Test
-    void return_fizz_given_3() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(3);
-        assertEquals("Fizz", actual);
-    }
-
-    @Test
-    void return_fizz_given_6() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(6);
-        assertEquals("Fizz", actual);
-    }
-
-    @Test
-    void return_buzz_given_5() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(5);
-        assertEquals("Buzz", actual);
-    }
-
-    @Test
-    void return_buzz_given_10() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(10);
-        assertEquals("Buzz", actual);
-    }
-
-    @Test
-    void return_fizzbuzz_given_15() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("FizzBuzz", fizzBuzz.convert(15));
-    }
-
-    @Test
-    void return_fizzbuzz_given_30() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("FizzBuzz", fizzBuzz.convert(30));
-    }
-
 }
