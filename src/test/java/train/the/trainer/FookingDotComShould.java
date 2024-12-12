efabf1d68e6e664cfaec949463b0a292afc18d99
John Wilson <mail@johnwilson.me.uk>
2024-12-12 20:03:05 +0000

    T - change to inline

--- src/test/java/train/the/trainer/FookingDotComShould.java
+++ src/test/java/train/the/trainer/FookingDotComShould.java
@@ -1,6 +1,7 @@
 package train.the.trainer;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -24,19 +25,48 @@ public class FookingDotComShould {
 
     @Test
     void update_the_movenpick_hotel() {
-
+        var expected = """
+            MovenPick, 9, 19
+            """;
         Hotel movenPick = new Hotel("MovenPick", 10, 20);
-        verifyNextDaysPrice(movenPick);
-
-
+        verifyNextDaysPrice(movenPick, expected);
+        
+    }
+    
+    @Test
+    void update_the_eden_hotel() {
+        var expected = """
+            Eden, 1, 1
+            """;
+        Hotel eden = new Hotel("Eden", 2, 0);
+        verifyNextDaysPrice(eden, expected);
+    }
+    
+    @Test
+    void update_the_mister_highland_hotel() {
+        Hotel misterHighland = new Hotel("Mister Highland", 5, 7);
+        verifyNextDaysPrice(misterHighland, "");
     }
 
-    private static void verifyNextDaysPrice(Hotel movenPick) {
+    @Test
+    void update_the_nh_leidseplein_hotel() {
+        Hotel nhLeidseplein = new Hotel("NH Leidseplein", 0, 80);
+        verifyNextDaysPrice(nhLeidseplein, "");
+    }
+    @Test
+    void update_the_room_mate_aitana_hotel() {
+        Hotel roomMateAitana = new Hotel("Room Mate Aitana", 15, 20);
+        verifyNextDaysPrice(roomMateAitana, "");
+    }
+
+   
+
+    private static void verifyNextDaysPrice(Hotel movenPick, String expected) {
         Hotel[] hotels = new Hotel[] {movenPick};
         FookingDotCom app = new FookingDotCom(hotels);
 
         app.update();
-
-        Approvals.verify(hotels[0]);
+        
+        Approvals.verify(hotels[0], new Options().inline(expected));
     }
 }
